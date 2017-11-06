06476a8b49b3383d63f89d43637dc9f6c7ec862f
Dan Callaghan <dcallagh@redhat.com>
2017-11-06 16:02:55 +1000

    tests: better assertion messages
    Currently the tests would always fail because of the 'Hypervisor' key
    being wrong. And moreover, the failure message was useless because it
    was impossible to see exactly which piece of the huge pile of data was
    wrong.
    
    Ultimately this is actually because of lxml. Even though it's dealing in
    XML (which is all Unicode) it "helpfully" converts ASCII strings down to
    str in Python 2. This means that most of the keys and values in the
    output of beaker-system-scan are str not unicode. However when we load
    the expected values with json.load() they come back as unicode not str.
    So when unittest tries to diff the two structures they appear completely
    different, even if there is only one tiny value incorrect.
    
    Change-Id: I87043254aebafc6826ea1f471b3cb772fbaa9861

--- tests/test_systemscan.py
+++ tests/test_systemscan.py
@@ -13,26 +13,66 @@ import os
 
 class SystemScanTest(unittest.TestCase):
 
+    longMessage = True
+    maxDiff = None
+
+    def assert_inventory_matches(self, expected, actual):
+        # TODO FIXME cannot test 'Hypervisor' currently because it depends on 
+        # the output of the hvm_detect program, which will always return 
+        # a value based on the machine running the tests -- it does not look 
+        # in the parsed XML from lshw, unlike everything else. So it's always 
+        # going to be wrong here in the tests.
+        del expected['Hypervisor']
+        del actual['Hypervisor']
+
+        expected_devices = expected.pop('Devices')
+        actual_devices = actual.pop('Devices')
+        for i, device in enumerate(expected_devices):
+            try:
+                self.assertEquals(device, actual_devices[i])
+            except IndexError:
+                self.fail('Missing device %s' % device)
+
+        expected_disks = expected.pop('Disk')['Disks']
+        actual_disks = actual.pop('Disk')['Disks']
+        for i, disk in enumerate(expected_disks):
+            try:
+                self.assertEquals(disk, actual_disks[i])
+            except IndexError:
+                self.fail('Missing disk %s' % disk)
+
+        expected_cpuflags = expected['Cpu']['CpuFlags']
+        actual_cpuflags = actual['Cpu']['CpuFlags']
+        self.assertItemsEqual(expected_cpuflags, actual_cpuflags)
+
+        for key in expected['Cpu']:
+            self.assertEquals(expected['Cpu'][key], actual['Cpu'][key],
+                    'Cpu %s should match' % key)
+
+        for key in expected:
+            self.assertEquals(expected[key], actual[key],
+                    '%s should match' % key)
+
     def test_read_inventory_hp_z420(self):
         inputxml = lxml.etree.parse('hp-z420.xml')
         expected = json.load(open('hp-z420.expected.json'))
         out = main.read_inventory(inputxml, arch='x86_64',
                 proc_cpuinfo=os.path.abspath('hp-z420.cpuinfo.txt'))
-        self.assertEquals(expected, out)
+        self.assert_inventory_matches(expected, out)
 
     def test_read_inventory_dell_pe2550(self):
         inputxml = lxml.etree.parse('dell-pe2550.xml')
         expected = json.load(open('dell-pe2550.expected.json'))
         out = main.read_inventory(inputxml, arch='x86_64',
                 proc_cpuinfo=os.path.abspath('dell-pe2550.cpuinfo.txt'))
-        self.assertEquals(expected, out)
+        self.assert_inventory_matches(expected, out)
 
     def test_read_inventory_ia64(self):
         inputxml = lxml.etree.parse('hp-rx1620.xml')
         expected = json.load(open('hp-rx1620.expected.json'))
         out = main.read_inventory(inputxml, arch='ia64',
                 proc_cpuinfo=os.path.abspath('hp-rx1620.cpuinfo.txt'))
-        self.assertEquals(expected, out)
+        self.assert_inventory_matches(expected, out)
 
     # https://bugzilla.redhat.com/show_bug.cgi?id=1249460
     def test_zero_values_are_excluded_from_USBID_key(self):
@@ -48,7 +88,7 @@ class SystemScanTest(unittest.TestCase):
         expected = json.load(open('s390-guest.expected.json'))
         out = main.read_inventory(inputxml, arch='s390x',
                 proc_cpuinfo=os.path.abspath('s390-guest.cpuinfo.txt'))
-        self.assertEquals(expected, out)
+        self.assert_inventory_matches(expected, out)
 
     # https://bugzilla.redhat.com/show_bug.cgi?id=1249466
     def test_diskspace_key_value(self):
@@ -75,14 +115,14 @@ class SystemScanTest(unittest.TestCase):
         inputxml = lxml.etree.parse('apm-mustang.xml')
         expected = json.load(open('apm-mustang.expected.json'))
         out = main.read_inventory(inputxml, arch='aarch64')
-        self.assertEquals(expected, out)
+        self.assert_inventory_matches(expected, out)
 
     def test_read_inventory_x86_kvm_guest(self):
         inputxml = lxml.etree.parse('x86-kvm-guest.xml')
         expected = json.load(open('x86-kvm-guest.expected.json'))
         out = main.read_inventory(inputxml, arch='x86_64',
                 proc_cpuinfo=os.path.abspath('x86-kvm-guest.cpuinfo.txt'))
-        self.assertEquals(expected, out)
+        self.assert_inventory_matches(expected, out)
 
 if __name__ == "__main__":
     unittest.main()
